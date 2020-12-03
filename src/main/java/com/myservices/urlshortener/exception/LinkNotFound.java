215f3014bfa5d5bfb740f54b0dddd40a11eddd2e
ivanrusanov <ivan.s.rusanov@gmail.com>
2020-12-03 08:59:24 +0300

    Properties file for heroku

--- src/main/java/com/myservices/urlshortener/exception/LinkNotFound.java
+++ src/main/java/com/myservices/urlshortener/exception/LinkNotFound.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class LinkNotFound extends ResponseStatusException {
+
     public LinkNotFound() {
         super(HttpStatus.NOT_FOUND, "Link not found");
     }
